873711cf2a4ec2351db43d05a8a87117b11a69d4
Aidar Zharassov <aidar.zharasov@meloman.kz>
2023-09-22 16:04:19 +0600

    fix

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import { readdir } from 'node:fs/promises';
+import { join } from 'node:path';
+
+const libFolder = 'libs';
+let libFiles = [];
 const init = async () => {
   const builds = await Bun.build({
     entrypoints: ['./hydrate.tsx'],
@@ -9,26 +14,34 @@ const init = async () => {
       whitespace: true,
     },
   });
+  libFiles = await getFiles(libFolder)
 }
 
 await init();
+
+async function getFiles( directoryPath ) {
+
+    try {
+        const fileNames = await readdir( directoryPath ); // returns a JS array of just short/local file-names, not paths.
+        const filePaths = fileNames.map( fn => join( directoryPath, fn ) );
+        return filePaths;
+    }
+    catch (err) {
+        console.error( err ); // depending on your application, this `catch` block (as-is) may be inappropriate; consider instead, either not-catching and/or re-throwing a new Error with the previous err attached.
+    }
+}
+
+
 
 export const server = Bun.serve({
   port: 3000,
   async fetch(req, server) {
     
     const { pathname } = new URL(req.url);
+    const filePath =  `${libFolder}${pathname}`;
     
-    if (pathname === "/underscore-umd.js" && req.method === "GET") {
-      return new Response(Bun.file(import.meta.dir + "/underscore-umd.js"), {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    };
-
-    if (pathname === "/backbone.js" && req.method === "GET") {
-      return new Response(Bun.file(import.meta.dir + "/backbone.js"), {
+    if (libFiles.includes(filePath) && req.method === "GET") {
+      return new Response(Bun.file(filePath), {
         headers: {
           'Content-Type': 'application/json',
         },
